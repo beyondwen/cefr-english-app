fc8f04037522412bd5a8772e61a96e6823d11e88
jack <11@gg.com>
2026-04-29 22:25:11 +0800

    chore: 收紧 ViewModel factory 类型检查

--- android/app/src/main/java/com/wenha/cefrenglish/app/SimpleViewModelFactory.kt
+++ android/app/src/main/java/com/wenha/cefrenglish/app/SimpleViewModelFactory.kt
@@ -4,5 +4,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 class SimpleViewModelFactory<T : ViewModel>(private val create: () -> T) : ViewModelProvider.Factory {
-    override fun <R : ViewModel> create(modelClass: Class<R>): R = create() as R
+    override fun <R : ViewModel> create(modelClass: Class<R>): R {
+        val viewModel = create()
+        require(modelClass.isAssignableFrom(viewModel::class.java)) {
+            "Cannot create ${modelClass.name} from ${viewModel::class.java.name}"
+        }
+        @Suppress("UNCHECKED_CAST")
+        return viewModel as R
+    }
 }
